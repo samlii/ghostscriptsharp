793ea5611a860c44eb8e23215a9123fa64b63d1a
George Mauer <gmauer@gmail.com>
2014-06-02 16:54:51 -0200

    don't fail on copy error

--- nuget-packaging/Tools/GetGsdll.ps1
+++ nuget-packaging/Tools/GetGsdll.ps1
@@ -1,4 +1,4 @@
 function Get-MoveGsDllString($toolsPath) {
 "
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command `"ls '`$(SolutionDir)\packages\GhostScriptSharp.*\Tools\gsdll32.dll' | Sort -Descending | Select -First 1 | cp -Destination '`$(TargetDir)' `""
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command `"ls '`$(SolutionDir)\packages\GhostScriptSharp.*\Tools\gsdll32.dll' | Sort -Descending | Select -First 1 | cp -Destination '`$(TargetDir)' -ErrorAction Continue`""
 }
